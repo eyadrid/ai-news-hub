356e1ac381d67a2600e1d2e84cba8208389c1646
Dridi Eya <eya.dridi@data-tricks.fr>
2026-03-24 19:26:38 +0100

    Creating the DB

---
+++ app/database/connection.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_database_url() -> str:
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("POSTGRES_HOST")
+    port = os.getenv("POSTGRES_PORT")
+    db = os.getenv("POSTGRES_DB")
+    return f"postgresql://{user}:{password}@{host}:{port}/{db}"
+
+engine = create_engine(get_database_url())
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_session():
+    return SessionLocal()
